dff14c4736deb8b3f98cfb4951e775edb1681f4c
Bruno Acosta <brunoeacosta07@gmail.com>
2022-08-08 12:49:42 -0300

    Pagina web Alura Geek segundo challenge front end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+  apiKey: "AIzaSyD38VpYs67IsCcYcvOfN5HTF99DJCVPkR0",
+  authDomain: "eccomerce-alura.firebaseapp.com",
+  projectId: "eccomerce-alura",
+  storageBucket: "eccomerce-alura.appspot.com",
+  messagingSenderId: "950570452997",
+  appId: "1:950570452997:web:be41cdb8fde1ae6ed9746c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore: {
+  apiKey: "AIzaSyD38VpYs67IsCcYcvOfN5HTF99DJCVPkR0",
+  authDomain: "eccomerce-alura.firebaseapp.com",
+  projectId: "eccomerce-alura",
+  storageBucket: "eccomerce-alura.appspot.com",
+  messagingSenderId: "950570452997",
+  appId: "1:950570452997:web:be41cdb8fde1ae6ed9746c"
+  }
 };
 
 /*
